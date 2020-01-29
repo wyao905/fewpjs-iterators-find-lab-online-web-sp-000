712163912fc86df4fbc08a9a9f1342f88c05aaca
Warren Yao <warrenyca905@gmail.com>
2020-01-29 23:38:46 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let won = arr.find(year => year.result === "W")
+  if(!!won) {
+    return won.year
+  } else {
+    return won
+  }
+}
